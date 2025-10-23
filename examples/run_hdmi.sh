0ea6f94e3e08738493b4c44ed531c563f9f331be
jc-bao <jc-bao@outlook.com>
2025-10-23 11:23:30 -0400

    ⚡ update default config for hdmi tracking task

--- examples/run_hdmi.sh
+++ examples/run_hdmi.sh
@@ -15,4 +15,6 @@ source ${HDMI_DIR}/.venv/bin/activate
 # - "rerun" for rerun viewer only
 # - "mujoco-rerun" for both mujoco and rerun (recommended for SPIDER)
 # - "mjlab-mujoco-rerun" for all three viewers (mjlab may conflict)
-python examples/run_hdmi.py viewer="mujoco-rerun" rerun_spawn=true horizon=0.8 knot_dt=0.2 ctrl_dt=0.04 num_samples=512  temperature=0.1 max_num_iterations=32 joint_noise_scale=0.2 first_ctrl_noise_scale=1.0 improvement_check_steps=1 improvement_threshold=0.02 #ctrl_dt=0.02 knot_dt=0.02 horizon=0.02 joint_noise_scale=0.0001 max_num_iterations=1
+# python examples/run_hdmi.py task=move_largebox viewer="mujoco-rerun" rerun_spawn=true horizon=0.8 knot_dt=0.2 ctrl_dt=0.04 num_samples=1024  temperature=0.1 max_num_iterations=32 joint_noise_scale=0.2 first_ctrl_noise_scale=1.0 improvement_check_steps=1 improvement_threshold=0.02 max_sim_steps=100 #ctrl_dt=0.02 knot_dt=0.02 horizon=0.02 joint_noise_scale=0.0001 max_num_iterations=1
+
+python examples/run_hdmi.py task=move_largebox joint_noise_scale=0.2 knot_dt=0.1 horizon=0.6 max_sim_steps=120
